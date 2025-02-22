6f60789f413cc4bb621dcc6a9058d4134cf51408
최정민 <bigdragon00@naver.com>
2025-02-22 15:02:43 +0900

    코드가 꼬임,, 수정 해야함

--- app.js
+++ app.js
@@ -30,17 +30,26 @@ const server = createServer((req, res) => {
     console.error("오류");
   }
   if (req.method === "POST") {
-    if (url === "/writeNewdocument") {
-      console.log("글 요청");
-      let list = [];
+    let content = "";
 
-      req.on("data", (frag) => {
-        list += frag; //배열에 stack하는 용도
-      });
+    req.on("data", (chunk) => {
+      content += chunk;
+    });
 
-      req.on("end", () => {
+    req.on("end", () => {
+      const data = parse(content);
+
+      if (url === "/writeNewdocument") {
+        console.log("글 요청");
+        let list = [];
+
+        req.on("data", (frag) => {
+          list += frag; //배열에 stack하는 용도
+        });
+
         if (list) {
           const doc = parse(list);
+          doc.date = new Date().toISOString();
           console.log("받은 글", doc);
 
           docList.push(doc);
@@ -48,10 +57,41 @@ const server = createServer((req, res) => {
         }
         res.writeHead(302, { Location: "/" }); // 입력 후 루트로 새로고침
         res.end();
-      });
+      }
+    });
+  } else if (url === "/editDocument") {
+    console.log("수정하기 불러옴");
 
-      return;
+    let newDoc = [];
+    for (let i = 0; i < docList.length; i++) {
+      let doc = docList[i];
+
+      if (doc.title === data.oldTitle) {
+        doc = {
+          title: data.newTitle,
+          write: data.newWrite,
+          date: new Date().toISOString()
+        };
+      }
+      newDoc.push(doc);
     }
+
+    docList = newDoc;
+    console.log("수정된 글", docList);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ success: true }));
+
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    req.on("end", () => {
+      const updateDoc = parse(data);
+      const index = docList.findIndex(
+        (doc) => doc.title === updateDoc.oldTitle
+      );
+    });
   }
 });
 
