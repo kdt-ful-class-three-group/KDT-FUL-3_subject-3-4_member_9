11cc8b541fbeca6f9f9dda9c4d687148e2104b0a
최정민 <bigdragon00@naver.com>
2025-03-01 23:20:44 +0900

    bodyPaser를 이용해야함

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import { createServer } from "http";
 import { readFileSync } from "fs";
 import { parse } from "querystring";
 import { express } from "express";
-
+import bodyPaser from "body-paser";
 
 const app = express();
-app.use(express.static('./'));
+app.use(express.static("./"));
 
 let docList = [];
 
@@ -32,47 +32,45 @@ app.post("/editDocument", (req, res) => {
   console.log("수정 요청");
   const data = req.body;
 
-    let newDoc = [];
-    for (let i = 0; i < docList.length; i++) {
-        let doc = docList[i];
+  let newDoc = [];
+  for (let i = 0; i < docList.length; i++) {
+    let doc = docList[i];
 
-        if (doc.title === data.oldTitle) {
-            doc = {
-            title: data.newTitle,
-            write: data.newWrite,
-            date: new Date().toLocaleString()
-            };
-        }
-        newDoc.push(doc);
+    if (doc.title === data.oldTitle) {
+      doc = {
+        title: data.newTitle,
+        write: data.newWrite,
+        date: new Date().toLocaleString()
+      };
     }
-    docList = newDoc;
-    console.log("수정된 글", docList);
-    res.redirect("/");
-} );
+    newDoc.push(doc);
+  }
+  docList = newDoc;
+  console.log("수정된 글", docList);
+  res.redirect("/");
+});
 
 app.post("/deleteDocument", (req, res) => {
-    console.log("삭제 요청");
-    const data = req.body;
-    const beforeDel = docList.length;
-    docList = docList.filter((doc) => doc.title !== data.title);
-    const deleted = beforeDel !== docList.length;
-    console.log("삭제 후 글 리스트", docList);
-    res.json({ success: deleted });
+  console.log("삭제 요청");
+  const data = req.body;
+  const beforeDel = docList.length;
+  docList = docList.filter((doc) => doc.title !== data.title);
+  const deleted = beforeDel !== docList.length;
+  console.log("삭제 후 글 리스트", docList);
+  res.json({ success: deleted });
 });
 
 app.use((req, res) => {
-    try{
-        res.status(404).send(readFileSync('./404.html', 'utf-8'));
-    } catch(error){
-        console.error("404 error",error);
-        res.status(404).send("404 페이지를 찾을 수 없습니다.");
+  try {
+    res.status(404).send(readFileSync("./404.html", "utf-8"));
+  } catch (error) {
+    console.error("404 error", error);
+    res.status(404).send("404 페이지를 찾을 수 없습니다.");
+  }
+});
 
-    }
-})
+let PORT = 8000;
 
-
-  let PORT = 8000;
-
-    createServer(app).listen(PORT, () => {
-        console.log(`http://localhost:${PORT} 에서 서버 구동 중`);
-    });
+createServer(app).listen(PORT, () => {
+  console.log(`http://localhost:${PORT} 에서 서버 구동 중`);
+});
